6e937c63d37d18c2f6347d699730c4ab5f440dca
Ishan Bhalla <ishanb1569@gmail.com>
2024-04-12 13:58:57 -0400

    second-last commit, documentation left

--- ishan/SQL/dml.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO Users VALUES 
-('craig', 'abc', 'client'),
-('daniel', '123', 'client');
--- ishan/src/routes/getRoutes.js
+++
@@ -1,5 +0,0 @@
-import express from 'express';
-import pool from '../pool.js';
-const router = express.Router();
-
-export default router;
